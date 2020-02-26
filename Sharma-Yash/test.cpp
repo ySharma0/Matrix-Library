10e353622008602327546685bbd5fe8281dd72ad
Yash Sharma <mr.sharmayash@outlook.com>
2020-02-26 01:57:33 -0800

    added loop to add vals to arrray

--- Sharma-Yash/test.cpp
+++ Sharma-Yash/test.cpp
@@ -3,10 +3,21 @@
 
 int main(){
 
-    Matrix m(3,3);
-    Matrix b(3,3);
+    Matrix m(4,3);
+    Matrix b(3,4);
 
     Matrix c(3,3);
+
+    int **matrix = m.getMatrix();
+    for(int i = 0; i < m.getRows(); i++){
+
+        for(int j = 0; j < m.getCols(); j++){
+            *(*(matrix+i)+j) = i*(j+1);
+        }
+
+    }
+    m.print();
+    b.print();
 
     c = m * b;
 
